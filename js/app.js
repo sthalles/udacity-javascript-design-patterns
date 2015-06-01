802c146fea53a77684c84c896c63f30157dce376
Thalles Silva <thalles753@gmail.com>
2015-05-31 22:26:46 -0300

    End of the thrid module

--- js/app.js
+++ js/app.js
@@ -1,42 +1,69 @@
-var Cat = function() {
+var cats = [
+    {
+        "name": "Consuelo", 
+        "clickCount": 0,
+        "imgSource": "img/cat1.jpg",
+		"nicknames": ["Gatinha"]
+    }, 
+	{
+        "name": "Puppy", 
+        "clickCount": 0,
+        "imgSource": "img/cat2.jpg",
+		"nicknames": ["Bixano"]
+    },
+    {
+        "name": "Genie Bouchard",
+        "clickCount": 0,
+        "imgSource": "img/cat3.jpg",
+		"nicknames": ["Scooby"]
+    } 
+];
+
+var Cat = function(data) {
 	// For simplicity, lets create our model inside the ViewModel, 
 	// the important thing to keep in mind is that Knockout
 	// will functionally separete the Model from the View.
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable("Tabby");
-	this.imgSource = ko.observable("img/cat1.jpg");
-	this.level = ko.observable("Newborn");
-	this.nicknames = ko.observableArray([
-		{name: "Consul"},
-		{name: "Bixano"},
-		{name: "Miao"},
-		{name: "Gatinho"}
-	]);
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSource = ko.observable(data.imgSource);
+	this.nicknames = ko.observableArray(data.nicknames);
 	// end of Model definition
 	
-	this.levels = ko.computed(function(){
-		if (this.clickCount() == 10) {
-			this.level("Infant");	
+	this.level = ko.computed(function(){
+		if (this.clickCount() >= 10) {
+			return "Infant";	
+		} else {
+			return "Newborn";
 		}
 		
 	}, this);
 }
 
 var ViewModel = function() {
+	var self = this; // self is the ViewModel Context
 	
-	this.currentCat = ko.observable(new Cat());
+	// observable array to keep track of the cats
+	this.catList = ko.observableArray([]);
+	
+	// iterate over the cat data and add one by one to the cat list
+	cats.forEach(function(catItem){
+		self.catList.push(new Cat(catItem)); // store each cat in the cat list
+	});
+	
+	// creates the first cat
+	this.currentCat = ko.observable(this.catList()[0]);
+	console.log(this.catList()[0]);
+	
+	this.updateImg = function(clickedItem) {
+		self.currentCat( clickedItem );
+	};
 	
 	// View Model
 	// Because Knockout will handle the View - Model syncronization
 	// the only time we need ViewModel methods is when we
 	// need to update the data.
 	this.incrementCounter = function() {
-		this.currentCat().clickCount( this.currentCat().clickCount() + 1 );
-	};
-	
-	var self = this;
-	this.removeNickname = function(){
-		self.currentCat().nicknames.remove(this);
+		self.currentCat().clickCount( self.currentCat().clickCount() + 1 );
 	};
 }
 
